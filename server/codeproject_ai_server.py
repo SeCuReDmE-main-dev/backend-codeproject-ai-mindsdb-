e7d28f3f6c49727878049ccefe7942964311ed8e
Celebrum <jeansebastienbeaulieuscrde.01@gmail.com>
2025-02-09 06:55:27 -0500

    Refactor server code to unify model handling and add home route for redirection

--- server/codeproject_ai_server.py
+++ server/codeproject_ai_server.py
@@ -1,6 +1,6 @@
-import os
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///codeproject_ai_server.db'
@@ -10,6 +10,10 @@ class AIModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(200), nullable=False)
+
+@app.route('/')
+def home():
+    return redirect(url_for('get_models'))
 
 @app.route('/models', methods=['GET'])
 def get_models():
@@ -25,6 +29,7 @@ def add_model():
     return jsonify({'message': 'Model added successfully'}), 201
 
 if __name__ == '__main__':
-    if not os.path.exists('codeproject_ai_server.db'):
-        db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    with app.app_context():
+        if not os.path.exists('codeproject_ai_server.db'):
+            db.create_all()
+        app.run(host='0.0.0.0', port=5000)
--- server/mindsdb_server.py
+++ server/mindsdb_server.py
@@ -1,30 +1,35 @@
-import os
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mindsdb_server.db'
 db = SQLAlchemy(app)
 
-class MindsDBModel(db.Model):
+class Model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(200), nullable=False)
 
-@app.route('/mindsdb_models', methods=['GET'])
-def get_mindsdb_models():
-    models = MindsDBModel.query.all()
+@app.route('/')
+def home():
+    return redirect(url_for('get_models'))
+
+@app.route('/models', methods=['GET'])
+def get_models():
+    models = Model.query.all()
     return jsonify([{'id': model.id, 'name': model.name, 'description': model.description} for model in models])
 
-@app.route('/mindsdb_models', methods=['POST'])
-def add_mindsdb_model():
+@app.route('/models', methods=['POST'])
+def add_model():
     data = request.get_json()
-    new_model = MindsDBModel(name=data['name'], description=data['description'])
+    new_model = Model(name=data['name'], description=data['description'])
     db.session.add(new_model)
     db.session.commit()
-    return jsonify({'message': 'MindsDB model added successfully'}), 201
+    return jsonify({'message': 'Model added successfully'}), 201
 
 if __name__ == '__main__':
-    if not os.path.exists('mindsdb_server.db'):
-        db.create_all()
-    app.run(host='0.0.0.0', port=5001)
+    with app.app_context():
+        if not os.path.exists('mindsdb_server.db'):
+            db.create_all()
+        app.run(host='0.0.0.0', port=5001)
